e03321be0def24658f28bd5dad82ed59e22fc1a3
aghaee55 <eam.univer@gmail.com>
2023-12-18 15:18:22 +0330

    Release python

--- Src/main.py
+++ Src/main.py
@@ -1,3 +1,18 @@
-print("Hi")
-print ("What is your name?")
-print("By")
+clc;
+clear;
+close all;
+Dataset=load('mydataset.txt');
+InputsData=Dataset(:,1:end-1);
+OutputData=Dataset(:,end);
+% inp=Dataset.dataset;
+ x=InputsData';
+t=OutputData';
+a=1;
+b=13;
+c=14;
+data.x=x;
+data.t=t;
+data.nx=size(x,1);
+data.nt=size(t,1);
+data.nSample=size(x,2);
+nVar=data.nx;       
